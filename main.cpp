2777dac9d715d6e009bfcba96552465d79d31562
Sam Scott <samueltscott@gmail.com>
2017-12-20 15:03:03 +0000

    a

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <SFML\Graphics.hpp>
+#include <Windows.h>
 #include "constants.h"
+#include "tiffio.h"
 
 struct Coord {
 	int x;
@@ -31,7 +33,7 @@ public:
 };
 
 // scans the whole image for clusters and stores them in the coords vector
-std::vector<Coord> findClusters(int *pixels, double tolerance, int width, int height) {
+std::vector<Coord> findClusters(std::vector<int> pixels, double tolerance, int width, int height) {
 	std::vector<Coord> coords;
 	int v, cx, cy;
 	int sum = 0;
@@ -77,45 +79,41 @@ std::vector<Coord> findClusters(int *pixels, double tolerance, int width, int he
 }
 
 int main() {
-	sf::Image image;
 	sf::Font font;
-
-	if (!image.loadFromFile("test.png")) {
-		return -1;
-	}
-	std::cout << "Image loaded." << std::endl;
-
-	if (!font.loadFromFile("font.ttf")) {
-		return -1;
-	}
-	
-	const int imageWidth = image.getSize().x;
-	const int imageHeight = image.getSize().y;
-
-	std::cout << "Width: " << imageWidth << std::endl;
-	std::cout << "Height: " << imageHeight << std::endl;
-
-	sf::RenderWindow window(sf::VideoMode(imageWidth, imageHeight), "Application");
-	window.setFramerateLimit(30);
-
-	sf::Text text;
-	text.setFont(font);
-	text.setFillColor(sf::Color::Black);
-	text.setCharacterSize(40);
-
-	int *pixels = new int[imageWidth * imageHeight];
-	bool processed = false;
-	int scanCount = 100;
-	int scanSize = 10;
-
-	sf::RectangleShape cluster(sf::Vector2f(scanSize - 1, scanSize - 1));
-	cluster.setFillColor(sf::Color::Red);
-
-	sf::RectangleShape pixel(sf::Vector2f(1, 1));
-
+	uint32 imageHeight;
+	uint32 imageWidth;
+	std::vector<int> newPixels;
 	std::vector<Coord> coords;
 
-	
+	TIFF* tif = TIFFOpen("test.tif", "r");
+	if (tif) {
+		tsize_t scanline;
+		tdata_t buf;
+		uint32 row;
+		uint32 col;
+		uint8* data;
+
+		TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &imageHeight);
+		scanline = TIFFScanlineSize(tif);
+		imageWidth = scanline;
+		buf = _TIFFmalloc(scanline);
+		for (row = 0; row < imageHeight; row++) {
+			TIFFReadScanline(tif, buf, row);
+			for (col = 0; col < scanline; col++) {
+				data = (uint8*)buf;
+				newPixels.push_back(data[col]);
+			}
+		}
+		_TIFFfree(buf);
+		TIFFClose(tif);
+	}
+
+	sf::RenderWindow window(sf::VideoMode(imageWidth, imageHeight), "Application");
+	sf::RectangleShape pixel(sf::Vector2f(1, 1));
+	sf::RectangleShape cluster(sf::Vector2f(10, 10));
+	cluster.setFillColor(sf::Color::Red);
+	window.setFramerateLimit(30);
+	bool processed = false;
 
 	while (window.isOpen()) {
 
@@ -128,137 +126,23 @@ int main() {
 		}
 
 		window.clear(sf::Color::White);
-		int i = 0;
 
 		for (int y = 0; y < imageHeight; y++) {
-
 			for (int x = 0; x < imageWidth; x++) {
-				pixels[i] = image.getPixel(x, y).b;
-				i += 1;
-			}
-		}
-
-		for (int y = 0; y < imageHeight; y++) {
-
-			for (int x = 0; x < imageWidth; x++) {
-				int value = image.getPixel(x, y).b;
-
-				if (value > 0) {
+				int value = newPixels[(y * imageWidth) + x];
+				if (value > -1) {
 					pixel.setFillColor(sf::Color(0, 0, value));
-					pixel.setPosition(x * (imageWidth / windowWidth), y * (imageHeight / windowHeight));
+					pixel.setPosition(x, y);
 					window.draw(pixel);
 				}
 			}
 		}
-
 		
 		if (!processed) {
-			
 			processed = true;
-			coords = findClusters(pixels, tolerance, imageWidth, imageHeight);
+			coords = findClusters(newPixels, tolerance, imageWidth, imageHeight);
 			std::cout << "Found " << coords.size() << " coords." << std::endl;
-
-			// start cluster logic
-
-			std::vector<Coord> checked;
-			Coord c;
-			int count = 0;
-
-			// use a while loop to make a recursive path finder which keeps going until there is nowhere else to go
-			for (int i = 0; i < coords.size(); i++) {
-
-				break;
-
-				for (int j = 0; j < coords.size(); j++) {
-
-					if (i == j) {
-						continue;
-					}
-
-					bool found = false;
-					for (int k = 0; k < checked.size(); k++) {
-						if (checked[k].x == coords[i].x || checked[k].y == coords[i].y) {
-							found = true;
-						}
-						if (checked[k].x == coords[j].x || checked[k].y == coords[j].y) {
-							found = true;
-						}
-					}
-
-					if (found) {
-						continue;
-					}
-
-					// right left
-					if (coords[i].x == coords[j].x + scanSize && coords[i].y == coords[j].y) { // right
-						count += 1;
-						c.x = coords[j].x + scanSize;
-						c.y = coords[j].y;
-						checked.push_back(c);
-						continue;
-					}
-					if (coords[i].x == coords[j].x - scanSize && coords[i].y == coords[j].y) { // left
-						count += 1;
-						c.x = coords[j].x - scanSize;
-						c.y = coords[j].y;
-						checked.push_back(c);
-						continue;
-					}
-
-					// up down
-					if (coords[i].x == coords[j].x && coords[i].y == coords[j].y + scanSize) { // down
-						count += 1;
-						c.x = coords[j].x;
-						c.y = coords[j].y + scanSize;
-						checked.push_back(c);
-						continue;
-					}
-					if (coords[i].x == coords[j].x && coords[i].y == coords[j].y - scanSize) { // up
-						count += 1;
-						c.x = coords[j].x;
-						c.y = coords[j].y - scanSize;
-						checked.push_back(c);
-						continue;
-					}
-
-					// diagonals
-					if (coords[i].x == coords[j].x + scanSize && coords[i].y == coords[j].y + scanSize) { // bottom right
-						count += 1;
-						c.x = coords[j].x + scanSize;
-						c.y = coords[j].y + scanSize;
-						checked.push_back(c);
-						continue;
-					}
-					if (coords[i].x == coords[j].x + scanSize && coords[i].y == coords[j].y - scanSize) { // top right
-						count += 1;
-						c.x = coords[j].x + scanSize;
-						c.y = coords[j].y - scanSize;
-						checked.push_back(c);
-						continue;
-					}
-					if (coords[i].x == coords[j].x - scanSize && coords[i].y == coords[j].y + scanSize) { // bottom left
-						count += 1;
-						c.x = coords[j].x - scanSize;
-						c.y = coords[j].y + scanSize;
-						checked.push_back(c);
-						continue;
-					}
-					if (coords[i].x == coords[j].x - scanSize && coords[i].y == coords[j].y - scanSize) { // top left
-						count += 1;
-						c.x = coords[j].x - scanSize;
-						c.y = coords[j].y - scanSize;
-						checked.push_back(c);
-						continue;
-					}
-				}
-			}
-
-			std::cout << "Found " << count << " clusters." << std::endl;
-
-			
 		}
-
-		// end cluster logic
 
 		for (int i = 0; i < coords.size(); i++) {
 			int x = coords[i].x * (imageWidth / windowWidth);
@@ -266,6 +150,110 @@ int main() {
 			cluster.setPosition(sf::Vector2f(x, y));
 			window.draw(cluster);
 		}
+
+		//	// start cluster logic
+
+		//	std::vector<Coord> checked;
+		//	Coord c;
+		//	int count = 0;
+
+		//	// use a while loop to make a recursive path finder which keeps going until there is nowhere else to go
+		//	for (int i = 0; i < coords.size(); i++) {
+
+		//		break;
+
+		//		for (int j = 0; j < coords.size(); j++) {
+
+		//			if (i == j) {
+		//				continue;
+		//			}
+
+		//			bool found = false;
+		//			for (int k = 0; k < checked.size(); k++) {
+		//				if (checked[k].x == coords[i].x || checked[k].y == coords[i].y) {
+		//					found = true;
+		//				}
+		//				if (checked[k].x == coords[j].x || checked[k].y == coords[j].y) {
+		//					found = true;
+		//				}
+		//			}
+
+		//			if (found) {
+		//				continue;
+		//			}
+
+		//			// right left
+		//			if (coords[i].x == coords[j].x + scanSize && coords[i].y == coords[j].y) { // right
+		//				count += 1;
+		//				c.x = coords[j].x + scanSize;
+		//				c.y = coords[j].y;
+		//				checked.push_back(c);
+		//				continue;
+		//			}
+		//			if (coords[i].x == coords[j].x - scanSize && coords[i].y == coords[j].y) { // left
+		//				count += 1;
+		//				c.x = coords[j].x - scanSize;
+		//				c.y = coords[j].y;
+		//				checked.push_back(c);
+		//				continue;
+		//			}
+
+		//			// up down
+		//			if (coords[i].x == coords[j].x && coords[i].y == coords[j].y + scanSize) { // down
+		//				count += 1;
+		//				c.x = coords[j].x;
+		//				c.y = coords[j].y + scanSize;
+		//				checked.push_back(c);
+		//				continue;
+		//			}
+		//			if (coords[i].x == coords[j].x && coords[i].y == coords[j].y - scanSize) { // up
+		//				count += 1;
+		//				c.x = coords[j].x;
+		//				c.y = coords[j].y - scanSize;
+		//				checked.push_back(c);
+		//				continue;
+		//			}
+
+		//			// diagonals
+		//			if (coords[i].x == coords[j].x + scanSize && coords[i].y == coords[j].y + scanSize) { // bottom right
+		//				count += 1;
+		//				c.x = coords[j].x + scanSize;
+		//				c.y = coords[j].y + scanSize;
+		//				checked.push_back(c);
+		//				continue;
+		//			}
+		//			if (coords[i].x == coords[j].x + scanSize && coords[i].y == coords[j].y - scanSize) { // top right
+		//				count += 1;
+		//				c.x = coords[j].x + scanSize;
+		//				c.y = coords[j].y - scanSize;
+		//				checked.push_back(c);
+		//				continue;
+		//			}
+		//			if (coords[i].x == coords[j].x - scanSize && coords[i].y == coords[j].y + scanSize) { // bottom left
+		//				count += 1;
+		//				c.x = coords[j].x - scanSize;
+		//				c.y = coords[j].y + scanSize;
+		//				checked.push_back(c);
+		//				continue;
+		//			}
+		//			if (coords[i].x == coords[j].x - scanSize && coords[i].y == coords[j].y - scanSize) { // top left
+		//				count += 1;
+		//				c.x = coords[j].x - scanSize;
+		//				c.y = coords[j].y - scanSize;
+		//				checked.push_back(c);
+		//				continue;
+		//			}
+		//		}
+		//	}
+
+		//	std::cout << "Found " << count << " clusters." << std::endl;
+
+		//	
+		//}
+
+		//// end cluster logic
+
+		
 
 		//text.setString(std::to_string(clusters[i].value));
 		//text.setPosition(x, y);
